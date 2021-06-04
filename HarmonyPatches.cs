e2bf726a973eb483c6102993c1cd7bb2bc6f6934
Mr_Chimick <mikechimick@gmail.com>
2021-06-03 19:31:09 -0700

    Initial commit

---
+++ HarmonyPatches.cs
@@ -0,0 +1,36 @@
+﻿using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace DropsAreAlwaysBiocoded
+{
+    [StaticConstructorOnStartup]
+    static class HarmonyPatches
+    {
+        static HarmonyPatches()
+        {
+            new Harmony("DropsAreAlwaysBiocoded").PatchAll();
+        }
+    }
+
+    [HarmonyPatch(typeof(PawnGenerator))]
+    [HarmonyPatch("GeneratePawn", typeof(PawnGenerationRequest))]
+    static class GeneratePawnPatch
+    {
+        static void Prefix(ref PawnGenerationRequest request)
+        {
+            if (request.Faction == null) return;
+            
+            if (request.Faction.HostileTo(Faction.OfPlayerSilentFail))
+            {
+                request.BiocodeApparelChance = 1f;
+                request.BiocodeWeaponChance = 1f;
+            }
+            else if (request.Faction.IsPlayer)
+            {
+                request.BiocodeApparelChance = 0f;
+                request.BiocodeWeaponChance = 0f;
+            }
+        }
+    }
+}
